7158f3dc1cee7e48299dfd47f1d5e3e46d25cdec
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-08-08 12:34:54 +0700

    fix validation paid

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -172,6 +172,7 @@ class OrderResource extends Resource
                         ->required()
                         ->numeric()
                         ->live()
+                        ->gte('total')
                         ->afterStateUpdated(function (Get $get, Set $set) {
                             self::updateTotals($get, $set);
                         }),
