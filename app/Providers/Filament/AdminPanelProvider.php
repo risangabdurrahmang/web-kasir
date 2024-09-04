9b980d34da57ff02f5aa3ee81f8a01d1a550913b
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-04 08:35:38 +0700

    add searchable select dropdown customer

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\ProductResource;
+use Awcodes\Overlook\OverlookPlugin;
+use Awcodes\Overlook\Widgets\OverlookWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
