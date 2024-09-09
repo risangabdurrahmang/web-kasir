3992b521e160bbd1e729b63b5a6ca8c15e58c9c9
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-09 09:21:07 +0700

    production setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, 'admin@gmail.com') && $this->hasVerifiedEmail();
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -44,10 +48,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function order(): HasMany
-    {
-        return $this->hashMany(Order::class);
     }
 }
