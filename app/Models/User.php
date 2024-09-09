526ad1e542baa38937f775bb02e17dbe1145886f
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-09 09:39:24 +0700

    add user seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,9 +45,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    public function order(): HasMany
-    {
-        return $this->hashMany(Order::class);
-    }
 }
