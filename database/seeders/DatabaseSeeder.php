9ad6364ec52dd2110b9714364b6678cc56ddb35c
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-19 10:31:09 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
-            'password' => 'password',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
         ]);
     }
 }
