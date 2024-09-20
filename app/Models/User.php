0b71a28b8dd440aeec4ffdbe79c6a5d6af84b9bb
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-20 08:12:00 +0700

    update user roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,15 @@
 
 namespace App\Models;
 
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use SoftDeletes, HasFactory, Notifiable, HasRoles;
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, 'admin@example.com') && $this->hasVerifiedEmail();
-    }
     /**
      * The attributes that are mass assignable.
      *
