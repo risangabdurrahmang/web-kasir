07d970556409f9f0b7ec13f79d69d4e3f3226de0
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-20 09:50:57 +0700

    remove required in email verified

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -42,8 +42,7 @@ class UserResource extends Resource implements HasShieldPermissions
                     ->preload()
                     ->searchable(),
                 Forms\Components\DateTimePicker::make('email_verified_at')
-                    ->default(now())
-                    ->required(),
+                    ->default(now()),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->maxLength(255)
