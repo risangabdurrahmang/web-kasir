7a3a3d8599371d578f27e2299e42a409adf52251
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-09-22 17:07:34 +0700

    remove model unguard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::unguard();
         OrderItem::observe(OrderItemObserver::class);
     }
 }
