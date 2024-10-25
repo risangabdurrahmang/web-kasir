491bc774de61441091f684003916dc9b0a813487
risangabdurrahmang <rabdurrahmang@gmail.com>
2024-10-25 13:55:12 +0700

    improve load pages with prefetching

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,21 @@ export default defineConfig({
             refresh: [...refreshPaths, "app/Livewire/**"],
         }),
     ],
+    build: {
+        minify: true,
+        sourcemap: false,
+
+        rollupOptions: {
+            output: {
+                manualChunks: (path) => {
+                    if (path.includes("codemirror")) {
+                        return "codemirror";
+                    }
+                    if (path.includes("node_modules")) {
+                        return "vendor";
+                    }
+                },
+            },
+        },
+    },
 });
